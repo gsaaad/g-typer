fa4bba388d4f4c67729d2937c815bb0475aadf8e
unknown <saad.george@live.com>
2025-02-07 08:05:24 -0500

    addition of styling of highscore

--- src/components/HighScoreCard/HighScoreCard.js
+++ src/components/HighScoreCard/HighScoreCard.js
@@ -1,49 +1,63 @@
 import "./HighScoreCard.css";
-import SaveCard from "../SaveCard/SaveCard";
 
-const HighScoreCard = ({ styleHighScoreComponent, highScore }) => {
-  const totalWinners = [
-    { name: "John", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    {
-      name: "Jane",
-      errorCount: 56,
-      successCount: 66,
-      wordsPerMinute: 100,
-    },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-    { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
-  ];
+const HighScoreCard = ({ styleHighScoreComponent }) => {
+  // let totalWinners = [
+  //   { name: "John", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   {
+  //     name: "Jane",
+  //     errorCount: 56,
+  //     successCount: 66,
+  //     wordsPerMinute: 100,
+  //   },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  //   { name: "Doe", errorCount: 56, successCount: 66, wordsPerMinute: 100 },
+  // ];
+  // set totalWinners to the value stored in localStorage
+  // localStorage.setItem("G-Typers", JSON.stringify(totalWinners));
+  const totalWinners = [];
+  const storedWinners = localStorage.getItem("G-Typers");
+  console.log("storedWinners", storedWinners);
+
+  const winnersFromStorage = JSON.parse(storedWinners);
+  if (Array.isArray(winnersFromStorage)) {
+    totalWinners.push(...winnersFromStorage);
+  } else {
+    console.error("Expected winners to be an array");
+  }
+
+  console.log("totalWinners", totalWinners);
+  // sort winners by wordsPerMinute in descending order
+  const sortedWinners = [...totalWinners].sort(
+    (a, b) => b.wordsPerMinute - a.wordsPerMinute
+  );
+
   return (
     <section className="high-score" style={styleHighScoreComponent}>
-      <h1 className="HighScore">HighScore</h1>
+      <h1 className="HighScore">Top HighScores</h1>
       <div>
         <div className="row high-score-header">
-          <p className="col-3 border-bottom border-2 border-primary p-2 font-weight-bold mb-0">
-            Name
-          </p>
-          <p className="col-3 border-bottom border-2 border-primary p-2 font-weight-bold mb-0">
-            Error Count
-          </p>
-          <p className="col-3 border-bottom border-2 border-primary p-2 font-weight-bold mb-0">
-            Success Count
-          </p>
-          <p className="col-3 border-bottom border-2 border-primary p-2 font-weight-bold mb-0">
-            Words Per Minute
-          </p>
+          <p className="col-2">Rank</p>
+          <p className="col-3">Name</p>
+          <p className="col-2">Error Count</p>
+          <p className="col-2">Success Count</p>
+          <p className="col-3">WPM</p>
         </div>
         <ul className="score-list">
-          {totalWinners.map((item, index) => (
-            <li className="row" key={`winner-${index}`}>
-              <p className="col-3 score-item">{item.name}</p>
-              <p className="col-3 score-item">{item.errorCount}</p>
-              <p className="col-3 score-item">{item.successCount}</p>
-              <p className="col-3 score-item">{item.wordsPerMinute}</p>
+          {sortedWinners.map((item, index) => (
+            <li className="row" key={`winner-${index + 1}`}>
+              <p className={`col-2 score-rank rank-${index + 1}`}>
+                Rank: {index + 1}
+              </p>
+              <p className="col-3">{item.name}</p>
+              <p className="col-2">{item.errorCount}</p>
+              <p className="col-2">{item.successCount}</p>
+              <p className="col-3">{item.wordsPerMinute}</p>
             </li>
           ))}
         </ul>
